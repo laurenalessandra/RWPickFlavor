6d063c4c9d47598eb1cd41a08c0926c60f6f398a
Lauren Simon <laurenalessandra@hotmail.com>
2019-09-16 13:34:34 +0200

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Lauren Simon on 16.09.19.
+//  Copyright © 2019 Lauren Simon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
